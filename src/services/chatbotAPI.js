efcd5c24e5df0a2e5425023d06b41163278911ed
Zia Uddin <ziamaryam2526@gmail.com>
2025-11-07 22:10:26 +0500

    Update chatbot API endpoint URL

--- src/services/chatbotAPI.js
+++ src/services/chatbotAPI.js
@@ -1,5 +1,5 @@
 export const askQuestion = async (question) => {
-  const res = await fetch("https://novatalk-od5jsaql.b4a.run/api/chatbot/ask", {
+  const res = await fetch("https://novatalk-v73wmeud.b4a.run/api/chatbot/ask", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ question }),
