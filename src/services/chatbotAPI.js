cb166217818f98e6e2864fa98f9526708843c6f7
ziauddin14 <ziamaryam2526@gmail.com>
2026-01-25 20:02:26 +0500

    refactor chatbot frontend

--- src/services/chatbotAPI.js
+++ src/services/chatbotAPI.js
@@ -1,9 +1,8 @@
 export const askQuestion = async (question) => {
-  const res = await fetch("https://novatalk-jpzpi4zr.b4a.run/api/chatbot/ask", {
+  const res = await fetch("http://localhost:4002/api/chatbot/ask", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ question }),
   });
   return res.json();
 };
-
