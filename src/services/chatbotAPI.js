5c1af103432093bff0521cf144fa2eed8a9a00fa
ziauddin14 <ziamaryam2526@gmail.com>
2026-01-25 20:18:16 +0500

    add live backend URL

--- src/services/chatbotAPI.js
+++ src/services/chatbotAPI.js
@@ -1,8 +1,11 @@
 export const askQuestion = async (question) => {
-  const res = await fetch("http://localhost:4002/api/chatbot/ask", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ question }),
-  });
+  const res = await fetch(
+    "https://novatalk-backend.onrender.com/api/chatbot/ask",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ question }),
+    },
+  );
   return res.json();
 };
